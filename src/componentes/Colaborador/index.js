5bcd5d6d7f632fb05ff2f2fdc71282d79e6879df
Gabriel Rocha Andrade Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-05-11 12:45:50 -0300

    Importando do github e preparando para deletar
    Importando o projeto Organo do github, alterando o componente do Colaborador para adicionar a funcionalidade de deletar um colaborador,
    Importando o pacote do react-icons para representar o ícone de deleção e preparando o ambiente para as próximas alterações

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,24 @@
+import { AiFillCloseCircle } from 'react-icons/ai'
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle
+        color="red"
+        size={25}
+        className="deletar"
+        onClick={aoDeletar}
+      />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  )
 }
 
 export default Colaborador
